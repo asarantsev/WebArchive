b3e0cf2ccdcfafe3ec3aba320829746eb11026e7
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2023-05-29 14:06:59 -0700

    Create simulator.py

---
+++ simulator.py
@@ -0,0 +1,19 @@
+from flask import flask, render_template, request
+import math
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+  if request_method == 'POST':
+    text = request.form['text']
+    result - calculation(text)
+    return render_template('simulator.html')
+  
+def calculation(text):
+  try: 
+    text = int(text)
+    return math.sin(text)
+  except:
+    return 'Enter Number'
+  
+if __name__ == '__main__':
+  app.run()
